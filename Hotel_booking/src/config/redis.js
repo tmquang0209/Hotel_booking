8daf22ced32086d0398ad356cb592e645a8c9da7
Trần Minh Quang <tmquang0209@gmail.com>
2024-09-27 13:51:45 +0700

    feat: Add Docker configuration for the hotel booking application

--- Hotel_booking/src/config/redis.js
+++ Hotel_booking/src/config/redis.js
@@ -1,7 +1,10 @@
+import "dotenv/config";
 import { createClient } from "redis";
 
+const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+
 const redisClient = await createClient({
-    url: "redis://localhost:6379/0",
+    url: `redis://${REDIS_HOST}:6379`,
 })
     .on("error", (error) => {
         console.error(error);
